986b472b743e5342cd6940476f180e7b294e673e
Nicolo Porter <porternicolo@gmail.com>
2023-05-24 00:57:13 +0800

    second commit
    - polished back end
    - added rankings

--- index.js
+++ index.js
@@ -1,51 +1,126 @@
-require("dotenv").config()
-const express = require('express'); 
+require("dotenv").config();
+const express = require('express');
 const { Sequelize, Model, DataTypes } = require('sequelize');
+const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const session = require('express-session');
+const app = express();
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.DATABASE_HOST,
-    process.env.DATABASE_PASSWORD,
-    {
-        host: 'localhost',
-        dialect: 'mysql',
-    });
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_HOST,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
 
-sequelize.authenticate().then(() => {
+sequelize
+  .authenticate()
+  .then(() => {
     console.log('Connection to DB is SUCCESSFUL.');
-}).catch(() => {
+  })
+  .catch(() => {
     console.log('ERROR! Connecting to DB.');
-})
+  });
 
 class User extends Model {}
 
-User.init ({
+User.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
     },
     username: {
-        type: new DataTypes.STRING,
-        allowNull: false
+      type: new DataTypes.STRING(),
+      allowNull: false
     },
     wpm: {
-        type: new DataTypes.INTEGER,
+      type: new DataTypes.INTEGER()
     }
-}, { 
+  },
+  {
     sequelize,
     modelName: 'User'
-});
+  }
+);
 
 console.log(User === sequelize.models.User);
 
-const app = express();
 app.use('/public', express.static('public'));
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: true
+}));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
-    res.render('pages/index');
+  res.render('pages/login');
 });
+
+app.post('/submit', (req, res) => {
+  const username = req.body.name;
+  req.session.username = username;
+  User.create({ username })
+    .then(() => {
+      console.log('Data saved successfully!');
+      res.redirect('/index');
+    })
+    .catch(err => {
+      console.error('Error saving data:', err);
+      res.send('Error saving data.');
+    });
+});
+
+app.get('/index', (req, res) => {
+    User.findAll()
+      .then((users) => {
+        const username = req.session.username;
+        res.render('pages/index', { users, username });
+      })
+      .catch((err) => {
+        console.error('Error retrieving data:', err);
+        res.send('Error retrieving data.');
+      });
+  });
+
+  app.post('/updateWPM', (req, res) => {
+    const { username, wpm } = req.body;
+  
+    User.update({ wpm: wpm }, { where: { username: username } })
+      .then(() => {
+        console.log('WPM updated successfully!');
+        console.log(wpm);
+        console.log(username);
+        res.redirect('/ranking');
+      })
+      .catch((err) => {
+        console.error('Error updating WPM:', err);
+        res.send('Error updating WPM.');
+      });
+  });
+
+  app.get('/ranking', (req, res) => {
+    User.findAll({
+      limit: 5, // Fetch only the top 5 users
+      order: [['wpm', 'DESC']] // Order by WPM in descending order
+    })
+      .then((users) => {
+        res.render('pages/ranking', { users });
+      })
+      .catch((err) => {
+        console.error('Error retrieving data:', err);
+        res.send('Error retrieving data.');
+      });
+  });
+
+  
+
 app.listen(4000, () => {
-    console.log('Server running on port 4000.');
+  console.log('Server running on port 4000.');
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,70 +1,79 @@
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random?minLength=250&maxLength=450'
-const quoteDisplayElement = document.querySelector('.quote-display')
-const quoteInputElement = document.querySelector('.container .quote-input')
-const timerElement = document.querySelector('.timer')
-const mistakesElement = document.querySelector('.mistake span')
-const wpmElement = document.querySelector('.WordPerMin span')
-const cpsElement = document.querySelector('.CharPerSec span')
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random?minLength=250&maxLength=450';
+const quoteDisplayElement = document.querySelector('.quote-display');
+const quoteInputElement = document.querySelector('.container .quote-input');
+const timerElement = document.querySelector('.timer');
+const mistakesElement = document.querySelector('.mistake span');
+const wpmElement = document.querySelector('.WordPerMin span');
+const cpsElement = document.querySelector('.CharPerSec span');
+const usernameElement = document.querySelector('#usernameElement');
 let timer;
 let maxTime = 60;
 let timeLeft = maxTime;
 let charIndex = mistakes = isTyping = 0;
+
 function getRandomQuote() {
   return fetch(RANDOM_QUOTE_API_URL)
     .then(response => response.json())
-    .then(data => data.content)
+    .then(data => data.content);
 }
+
 async function renderNewQuote() {
-  const quote = await getRandomQuote()
+  const quote = await getRandomQuote();
   quoteDisplayElement.innerHTML = '';
   quote.split('').forEach(char => {
-      console.log(char);
-      let span = `<span>${char}</span>`
-      quoteDisplayElement.innerHTML += span;
+    let span = `<span>${char}</span>`;
+    quoteDisplayElement.innerHTML += span;
   });
   quoteDisplayElement.querySelectorAll("span")[0].classList.add("active");
   document.addEventListener("keydown", () => quoteInputElement.focus());
   quoteDisplayElement.addEventListener("click", () => quoteInputElement.focus());
 }
+
 function initTyping() {
   let characters = quoteDisplayElement.querySelectorAll("span");
   let typedChar = quoteInputElement.value.split("")[charIndex];
   if (charIndex < characters.length - 1 && timeLeft > 0) {
-      if (!isTyping) {
-          timer = setInterval(startTimer, 1000);
-          isTyping = true;
+    if (!isTyping) {
+      timer = setInterval(startTimer, 1000);
+      isTyping = true;
+    }
+    if (typedChar == null) {
+      if (charIndex > 0) {
+        charIndex--;
+        if (characters[charIndex].classList.contains("incorrect")) {
+          mistakes--;
+        }
+        characters[charIndex].classList.remove("correct", "incorrect");
       }
-      if (typedChar == null) {
-          if (charIndex > 0) {
-              charIndex--;
-              if (characters[charIndex].classList.contains("incorrect")) {
-                  mistakes--;
-              }
-              characters[charIndex].classList.remove("correct", "incorrect");
-          }
+    } else {
+      if (characters[charIndex].innerText == typedChar) {
+        characters[charIndex].classList.add("correct");
       } else {
-          if (characters[charIndex].innerText == typedChar) {
-              characters[charIndex].classList.add("correct");
-          } else {
-              mistakes++;
-              characters[charIndex].classList.add("incorrect");
-          }
-          charIndex++;
+        mistakes++;
+        characters[charIndex].classList.add("incorrect");
       }
-      characters.forEach(span => span.classList.remove("active"));
-      characters[charIndex].classList.add("active");
+      charIndex++;
+    }
+    characters.forEach(span => span.classList.remove("active"));
+    characters[charIndex].classList.add("active");
 
-      let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-      wpm = wpm < 0 || !wpm || wpm === Infinity ? 0: wpm;
+    let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+    wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
 
-      wpmElement.innerText = wpm;
-      mistakesElement.innerText = mistakes;
-      cpsElement.innerText = charIndex - mistakes;
+    wpmElement.innerText = wpm;
+    mistakesElement.innerText = mistakes;
+    cpsElement.innerText = charIndex - mistakes;
   } else {
-      clearInterval(timer);
-      quoteInputElement.value = "";
+    clearInterval(timer);
+    quoteInputElement.value = "";
+
+    // Send the user's name and WPM to the server
+    const username = usernameElement.innerText;
+    const wpm = wpmElement.innerText;
+    sendWPMToServer(username, wpm);
   }
 }
+
 function startTimer() {
   if (timeLeft > 0) {
     timeLeft--;
@@ -73,8 +82,38 @@ function startTimer() {
     wpmElement.innerText = wpm;
   } else {
     clearInterval(timer);
+
+    // Timer ended, send the user's name and WPM to the server
+    const username = usernameElement.innerText;
+    const wpm = wpmElement.innerText;
+    sendWPMToServer(username, wpm);
   }
 }
+
+function sendWPMToServer(username, wpm) {
+  fetch('/updateWPM', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: usernameElement.innerText,
+      wpm: wpmElement.innerText
+    })
+  })
+    .then(response => response.text())
+    .then(data => {
+      console.log(usernameElement.innerText);
+      console.log(wpmElement.innerText);
+      console.log(data); // Log the response from the server
+
+      window.location.href = '/ranking';
+    })
+    .catch(error => {
+      console.error('Error updating WPM:', error);
+    });
+}
+
 function resetGame() {
   renderNewQuote();
   clearInterval(timer);
@@ -89,4 +128,3 @@ function resetGame() {
 
 renderNewQuote();
 quoteInputElement.addEventListener("input", initTyping);
-
